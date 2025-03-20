af34486cb608429969572f2e0a57b0e3eee46578
Farzeen <112006403+farzeenferoseee@users.noreply.github.com>
2025-03-20 21:25:14 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ diastolic_bp = st.number_input("Diastolic Blood Pressure", min_value=0, max_valu
 cholesterol = st.number_input("Cholesterol", min_value=0, max_value=200, value=100)
 
 # Empty DataFrame with the features expected by the model
-input_data = pd.DataFrame(columns=feature_names, index=[0]).fillna(0) 
+input_data = pd.DataFrame([[age, systolic_bp, diastolic_bp, cholesterol]], columns=feature_names, index=[0]).fillna(0) 
 
 input_data['Age'] = age
 input_data['Systolic Blood Pressure'] = systolic_bp
