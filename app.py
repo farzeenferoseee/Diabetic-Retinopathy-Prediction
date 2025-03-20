66effd64d3350ef806e8768c2156601d1a26d899
Farzeen <112006403+farzeenferoseee@users.noreply.github.com>
2025-03-20 21:58:22 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -21,32 +21,23 @@ systolic_bp = st.number_input("Systolic Blood Pressure", min_value=0, max_value=
 diastolic_bp = st.number_input("Diastolic Blood Pressure", min_value=0, max_value=140, value=80)
 cholesterol = st.number_input("Cholesterol", min_value=0, max_value=200, value=100)
 
-def predict_prognosis():
-  # Create a dictionary with the input data
-  input_data_dict = {
-    'Age': [age],
-    'Systolic Blood Pressure': [systolic_bp],
-    'Diastolic Blood Pressure': [diastolic_bp],
-    'Cholesterol': [cholesterol]
-  }
+# Empty DataFrame with the features expected by the model
+input_data = pd.DataFrame([[age, systolic_bp, diastolic_bp, cholesterol]], index=[0]).fillna(0)
 
-  # Create the input DataFrame with the correct feature order
-  input_data = pd.DataFrame(input_data_dict, columns=feature_names)
+input_data['age'] = age
+input_data['systolic_bp'] = systolic_bp
+input_data['diastolic_bp'] = diastolic_bp
+input_data['cholesterol'] = cholesterol
 
-  #Scale input data
-  input_data_scaled = scaler.transform(input_data)
+#Scale input data
+input_data_scaled = scaler.transform(input_data)
 
-  # Make prediction
-  prognosis = model.predict(input_data_scaled)[0]
-  return prognosis
+# Make prediction
+prognosis = model.predict(input_data_scaled)[0]
 
-def print_prognosis(prognosis):
-  if prognosis == 1:
-    st.write("This patient is predicted to have diabetic retinopathy.")
-  else:
-    st.write("It is predicted that this passenger does not have diabetic retinopathy.")
+if prognosis == 1:
+  st.write("This patient is predicted to have diabetic retinopathy.")
+else:
+  st.write("It is predicted that this passenger does not have diabetic retinopathy.")
 
-if st.button("Predict"):
-  predict_prognosis()
-  print_prognosis(prognosis)
 
